25918b12c5743e5cdb18984380a3793a5560e780
Lanmaomi <hyf4053@hotmail.com>
2023-02-19 09:45:18 +0800

    新增TOP初始化

--- src/scripts/instances/raids/TheOmegaProtocolUltimate.cpp
+++ src/scripts/instances/raids/TheOmegaProtocolUltimate.cpp
@@ -1,18 +1,50 @@
 #include <ScriptObject.h>
 #include <Territory/InstanceContent.h>
+#include <Manager/RNGMgr.h>
+
+#include <Actor/EventObject.h>
+#include <Actor/Player.h>
+#include <Actor/BNpc.h>
+
+#include <ServerMgr.h> //aka. WorldServer.h
+
+#include <Network/GamePacket.h>
+#include <Network/GameConnection.h>
+#include <Network/PacketDef/Zone/ServerZoneDef.h>
 
 using namespace Sapphire;
+using namespace Sapphire::Network::Packets;
 
 class TheOmegaProtocolUltimate :
   public Sapphire::ScriptAPI::InstanceContentScript
 {
+private:
+  enum Omegas : int
+  {
+    Omega = -1,
+    OmegaM = -1,
+    OmegaF = -1,
+    OpticalUnit = -1
+  };
+
+  enum Sequence : uint8_t
+  {
+    Seq1 = 1,
+    Seq2 = 3,
+    Seq3 = 7,
+    Seq4 = 23,
+    Seq5 = 31,
+    SeqFinish = 255
+  };
+
+public:
   TheOmegaProtocolUltimate():
-    Sapphire::ScriptAPI::InstanceContentScript( 30124 )
+    Sapphire::ScriptAPI::InstanceContentScript(30124)
   {}
 
   void onInit(InstanceContent& instance) override
   {
-    instance.registerEObj("Exit", 2000139, 0, 4, { 0.000000f, 0.000000f, -20.000000f }, 1.000000f, 0.000000f);
+   
   }
 
   void onUpdate(InstanceContent& instance, uint64_t tickCount) override
